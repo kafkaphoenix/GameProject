7aa88bb93b0c9b49eb570063121d980844769fc8
kafkaphoenix <jaguilerapuerta@gmail.com>
2024-09-02 01:04:41 +0200

    feat: bump engine to 1.2.0 and expand example

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
-#include "engine.h"
+#include <gameengine/engine.h>
+#include <glm/glm.hpp>
+#include <gameengine/utils.h>
+#include <iostream>
 
 int main() {
     engine::Engine engine;
     engine.start();
+    std::cout << "After engine started I can do other things, like using third party libraries exposed by the engine" << std::endl;
+    glm::vec3 v(1.0f, 0.0f, 0.0f);
+    std::cout << "Using glm: " << v.x << " " << v.y << " " << v.z << std::endl;
+    APP_ASSERT(1 != 1, "Trying macro");
     return 0;
 }
